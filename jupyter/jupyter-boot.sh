bf65a00a3cc02ec9d0817713d27dba752a09d46c
ziix <proooogram@gmail.com>
2024-01-03 15:03:30 +0800

    Update jupyter-boot.sh

--- jupyter/jupyter-boot.sh
+++ jupyter/jupyter-boot.sh
@@ -4,4 +4,4 @@
 /app/miniconda3/bin/python -c "from notebook.auth.security import set_password;  set_password(password='z') "
 
 #启动jupyter
-nohup /app/miniconda3/bin/jupyter --ip=0.0.0.0 &
+nohup /app/miniconda3/bin/jupyter notebook --ip=0.0.0.0 &
