38c3cbd99bfbd158ca6140359f6b91c385ac7f7e
ziix <proooogram@gmail.com>
2024-01-03 15:01:40 +0800

    Add new file

---
+++ jupyter/jupyter-boot.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#设置jupyter密码为z
+/app/miniconda3/bin/python -c "from notebook.auth.security import set_password;  set_password(password='z') "
+
+#启动jupyter
+nohup /app/miniconda3/bin/jupyter --ip=0.0.0.0 &
